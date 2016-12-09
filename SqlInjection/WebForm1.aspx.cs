ed69d7798f477510959d7d3fe9ef40c3a236b86a
Venkata Sreedhar <Venkata Sreedhar>
2016-12-08 20:17:51 -0800

    SqlInjection

---
+++ SqlInjection/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SqlInjection
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection con = new SqlConnection(CS))
+                {
+                    string Command = "Select * from tblProductInventory WHERE ProductName like '" + TextBox1.Text + "%'";
+                    SqlCommand cmd = new SqlCommand(Command, con);
+                    con.Open();
+                    GridView1.DataSource = cmd.ExecuteReader();
+                    GridView1.DataBind();               
+            }
+        }
+    }
+}
