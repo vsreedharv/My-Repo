7b7243242d0c5d38616be2d5358c56a8f53af0ec
Venkata Sreedhar <Venkata Sreedhar>
2016-12-31 20:23:10 -0800

    Reverse String Program

---
+++ ReverseString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String value;
+            Console.WriteLine("Please Enter a String: ");
+            value = Console.ReadLine();
+            char[] x = value.ToCharArray();
+            char[] y = new char[x.Length];
+            if (value!=null)
+            {
+                for(int i=0; i<x.Length;)
+                {
+                    for(int j = value.Length-1; j>=0; j--)
+                    {
+                        y[j] = x[i];
+                        i++;
+                    } 
+                }
+                Console.WriteLine("The reversed string is: ");
+                for(int i=0; i<x.Length;i++)
+                {
+                    Console.Write(y[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
