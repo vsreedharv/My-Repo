f3703764a6560fc22af4f90c09d7b194f9a7cec8
Venkata Sreedhar <Venkata Sreedhar>
2017-01-01 15:58:23 -0800

    Program for finding factorial of a number

---
+++ Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find its Factorial: ");
+            int n = int.Parse(Console.ReadLine());
+            if(n>0)
+            {
+                for(int i=n-1; i>0; i--)
+                {
+                    n = n*i;
+                }
+            }
+
+            Console.WriteLine("The Factorial of is " + n);
+            Console.ReadLine();
+
+        }
+    }
+}
