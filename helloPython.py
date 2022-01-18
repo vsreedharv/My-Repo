73a3133f4345a5abdb4d7eefd8d57aa4f46acf02
vsreedharv <vsvcloud@gmail.com>
2022-01-18 00:12:00 -0800

    Hello python world!

---
+++ helloPython.py
@@ -0,0 +1,13 @@
+x="Hello Python world!"
+print(x)
+
+List_DataStructures = ["DC", "Boston", "Chicago", "Florida", "New York", "Detroit", "Dallas", "Phoenix", "Seattle", "San Francisco", "San Diego"]
+print("List example: ", List_DataStructures)
+
+Dictionary_DataStructures = {"Seattle":425, "Palo Alto": 650, "Detroit": 248}
+print("Dictionary example", Dictionary_DataStructures["Detroit"])
+
+Sets_DataStructures = (11121991, 12111991, 19911112, 19911211, 12111991, 12111991, 12111991)
+print("Sets example", Sets_DataStructures)
+
+
